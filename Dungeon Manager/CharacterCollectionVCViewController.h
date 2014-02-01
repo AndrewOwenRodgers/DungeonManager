078c1932782c3a52ab63f73d857a7935d61ad665
AndrewOwenRodgers <owenrhyfelwr@gmail.com>
2014-01-31 16:16:59 -0800

    Finished more class architecture and some storyboard segues.

---
+++ Dungeon Manager/CharacterCollectionVCViewController.h
@@ -0,0 +1,16 @@
+//
+//  CharacterCollectionVCViewController.h
+//  Dungeon Manager
+//
+//  Created by Andrew Rodgers on 1/31/14.
+//  Copyright (c) 2014 EnhanceDailyLife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharacterCollectionVCViewController : UIViewController
+
+@property (strong, nonatomic) NSArray *characters;
+@property (nonatomic) BOOL isPCVC;
+
+@end
---
+++ Dungeon Manager/CharacterCollectionVCViewController.m
@@ -0,0 +1,38 @@
+//
+//  CharacterCollectionVCViewController.m
+//  Dungeon Manager
+//
+//  Created by Andrew Rodgers on 1/31/14.
+//  Copyright (c) 2014 EnhanceDailyLife. All rights reserved.
+//
+
+#import "CharacterCollectionVCViewController.h"
+
+@interface CharacterCollectionVCViewController ()
+
+@end
+
+@implementation CharacterCollectionVCViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
