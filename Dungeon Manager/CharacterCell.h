68d0f303793e07855cb8da25dc474a45b332e105
AndrewOwenRodgers <owenrhyfelwr@gmail.com>
2014-02-01 22:20:09 -0800

    UI work on the CharacterCollectionViewController and CharacterCell classes.

---
+++ Dungeon Manager/CharacterCell.h
@@ -0,0 +1,13 @@
+//
+//  CharacterCell.h
+//  Dungeon Manager
+//
+//  Created by Andrew Rodgers on 2/1/14.
+//  Copyright (c) 2014 EnhanceDailyLife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharacterCell : UICollectionViewCell
+
+@end
---
+++ Dungeon Manager/CharacterCell.m
@@ -0,0 +1,31 @@
+//
+//  CharacterCell.m
+//  Dungeon Manager
+//
+//  Created by Andrew Rodgers on 2/1/14.
+//  Copyright (c) 2014 EnhanceDailyLife. All rights reserved.
+//
+
+#import "CharacterCell.h"
+
+@implementation CharacterCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
