952fdbb1c179afb9a8b4e8b7ed7d617109f274ee
AndrewOwenRodgers <owenrhyfelwr@gmail.com>
2014-02-06 11:42:48 -0800

    Began UI and class architecture for GameCharacterVC

---
+++ Dungeon Manager/GameCharacterVC.h
@@ -0,0 +1,13 @@
+//
+//  GameCharacterVC.h
+//  Dungeon Manager
+//
+//  Created by Andrew Rodgers on 2/6/14.
+//  Copyright (c) 2014 EnhanceDailyLife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameCharacterVC : UIViewController
+
+@end
---
+++ Dungeon Manager/GameCharacterVC.m
@@ -0,0 +1,34 @@
+//
+//  GameCharacterVC.m
+//  Dungeon Manager
+//
+//  Created by Andrew Rodgers on 2/6/14.
+//  Copyright (c) 2014 EnhanceDailyLife. All rights reserved.
+//
+
+#import "GameCharacterVC.h"
+
+@interface GameCharacterVC ()
+
+@end
+
+@implementation GameCharacterVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backButton:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
